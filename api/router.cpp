ed9c60a84cd9322cd5bda80ee831766c2981d0ff
Sean Donnellan <darkdonno@gmail.com>
2013-07-07 10:00:52 +0930

    Initial version of the Router classes.
    The goal is to provide a simpler way for describing what functions should
    be called when given a certain components of a path/URI.

---
+++ api/router.cpp
@@ -0,0 +1,166 @@
+//===----------------------------------------------------------------------===//
+//
+// NAME         : Router
+// COPYRIGHT    : (c) 2013 Sean Donnellan. All Rights Reserved.
+// LICENSE      : The MIT License (see LICENSE.txt for details)
+//
+//===----------------------------------------------------------------------===//
+
+#include "router.hpp"
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+Router & Router::operator [](const char* term)
+{
+  auto route = myRoutes.insert(std::make_pair(std::string(term), Router()));
+  return route.first->second;
+}
+
+void Router::operator()(const char* term) const
+{
+  // :TODO: Fix this so it doesn't die if there were placeholders.
+  auto result = myRoutes.find(term);
+  if (result != myRoutes.end())
+  {
+    if (result->second.mySelfFunction) result->second.mySelfFunction();
+  }
+}
+
+void Router::operator()(const std::vector<std::string>& terms) const
+{
+  std::vector<std::string> placeholders;
+
+  // True if the last term is a place holder.
+  bool lastTermIsPlaceholder = false;
+  const Router* router = this;
+  for (auto term = std::begin(terms); term != std::end(terms); ++term)
+  {
+    // The current router takes an place-holder, so the "term" could be
+    // anything.
+    if (router->myPlaceholderFunction)
+    {
+      placeholders.push_back(*term);
+      ++term;
+
+      // The last term was just a place-holder so stop.
+      if (term == std::end(terms))
+      {
+        lastTermIsPlaceholder = true;
+        break;
+      }
+
+      // Fall through, as the term shold be looked up in myRoutes now.
+    }
+
+    const auto result = router->myRoutes.find(*term);
+    if (result == router->myRoutes.end())
+    {
+      // This probably needs to test if its another place-holder in support of
+      // allowing two of them in a row.
+      //
+      // TODO: Throw an "unknown_route_exception".
+    }
+    else
+    {
+      router = &result->second;
+    }
+  }
+
+  if (placeholders.empty() && router->mySelfFunction)
+  {
+    router->mySelfFunction();
+  }
+  else if (lastTermIsPlaceholder && router->myPlaceholderFunction)
+  {
+    router->myPlaceholderFunction(placeholders);
+  }
+  else if (router->myPlaceholderSelfFunction)
+  {
+    router->myPlaceholderSelfFunction(placeholders);
+  }
+  //else throw an exception unknown_route_exception
+}
+
+RouterWithPlaceholder Router::operator [](placeholder_t)
+{
+  return RouterWithPlaceholder(*this, true);
+}
+
+Router & Router::operator =(CallFunction function)
+{
+  mySelfFunction = function;
+  return *this;
+}
+
+RouterWithPlaceholder &
+RouterWithPlaceholder::operator =(Router::CallPlaceholderFunction function)
+{
+  if (isPlaceholder)
+  {
+    myRouter.myPlaceholderFunction = function;
+  }
+  else
+  {
+    myRouter.myPlaceholderSelfFunction = function;
+  }
+  return *this;
+}
+
+
+#ifdef ROUTER_ENABLE_TESTING
+
+void api_information() { puts(__FUNCSIG__); }
+void repositories_list() { puts(__FUNCSIG__); }
+void repository_information(const std::vector<std::string>& arguments)
+{
+  std::cout << "repository_information for \""  << arguments[0] << '"'
+            << std::endl;
+}
+
+void repository_tags(const std::vector<std::string>& arguments)
+{ puts(__FUNCSIG__); }
+
+void repository_branches(const std::vector<std::string>& arguments)
+{ puts(__FUNCSIG__); }
+
+void repository_tag(const std::vector<std::string>& arguments)
+{
+  std::cout << "tag \"" << arguments[1] << "\" for \""  << arguments[0] << '"'
+            << std::endl;
+}
+
+int main()
+{
+  Router router;
+  router["api"] = api_information;
+  router["api"]["repos"] = repositories_list;
+  router["api"]["repos"][Router::placeholder] = repository_information;
+  router["api"]["repos"][Router::placeholder]["tags"] = repository_tags;
+  router["api"]["repos"][Router::placeholder]["branches"] = repository_branches;
+
+  router["api"]["repos"][Router::placeholder]["tags"][Router::placeholder] =
+    repository_tag;
+
+  // TODO: Support setting up an alias.
+  // router["api"]["repositories"] = router["api"]["repos"];
+
+  // This needs working...
+  router("api");
+
+  // TODO: varadic arguments would be nice to support router("api", "repos")
+  std::vector<std::string> arguments;
+  arguments.push_back("api");
+  arguments.push_back("repos");
+  arguments.push_back("anything");
+  arguments.push_back("tags");
+  arguments.push_back("something");
+  router(arguments);
+
+  return 0;
+}
+#endif
+
+//===--------------------------- End of the file --------------------------===//
---
+++ api/router.hpp
@@ -0,0 +1,101 @@
+#ifndef ROUTER_HPP_
+#define ROUTER_HPP_
+//===----------------------------------------------------------------------===//
+//
+// NAME         : Router
+// COPYRIGHT    : (c) 2013 Sean Donnellan. All Rights Reserved.
+// LICENSE      : The MIT License (see LICENSE.txt for details)
+// DESCRIPTION  :
+//
+// Provides an simple and easy way of managing the routing from the components
+// of a path to a function.
+//
+// For example:
+//   /api/books - You want to map this to a list_books function.
+//   /api/books/<name> - You want to map this to an about_book function.
+//
+// Usage:
+//   Router router;
+//   router["api"]["books"] = list_books
+//   router["api"]["books"][Router::placeholder] = about_book
+//
+// Concepts:
+//   The placeholder is used where you want to allow any string to be used and
+//   it will be passed to the function being called.
+//
+//   In the example above, "placeholder" would be the name of the book.
+//
+//   Once a placeholder is used, the callback function accepts a std::vector of
+//   std::string.
+//
+//===----------------------------------------------------------------------===//
+
+#include <string>
+#include <map>
+#include <vector>
+
+class RouterWithPlaceholder;
+class Router
+{
+public:
+  typedef void (* CallFunction)(void);
+  typedef void (* CallPlaceholderFunction)(const std::vector<std::string>&);
+
+  Router() : mySelfFunction(nullptr), myPlaceholderFunction(nullptr) {}
+
+  // Used to indicate that this route takes a placeholder and passes it to the
+  // function.
+  typedef int placeholder_t;
+  static const int placeholder = 0;
+
+  // Methods for configuring the routing information.
+  Router & operator =(CallFunction function);
+  Router & operator [](const char* term);
+  RouterWithPlaceholder operator [](placeholder_t);
+
+  // Methods for invoking the functions.
+  void operator()(const char* term) const;
+  void operator()(const std::vector<std::string>& terms) const;
+
+private:
+  friend class RouterWithPlaceholder;
+
+  // The function to call
+  union
+  {
+    CallPlaceholderFunction myPlaceholderSelfFunction;
+    CallFunction mySelfFunction;
+  };
+
+  // The function to call if the next argument is a place holder (match
+  // anything) value.
+  CallPlaceholderFunction myPlaceholderFunction;
+
+  std::map<std::string, Router> myRoutes;
+};
+
+class RouterWithPlaceholder
+{
+public:
+  RouterWithPlaceholder(Router& router, bool isPlaceholder)
+  : myRouter(router), isPlaceholder(isPlaceholder) {}
+
+  // Methods for configuring the routing information.
+  RouterWithPlaceholder & operator =(Router::CallPlaceholderFunction function);
+  RouterWithPlaceholder operator [](const char* term)
+  {
+    return RouterWithPlaceholder(myRouter.operator [](term), false);
+  }
+  RouterWithPlaceholder operator [](Router::placeholder_t v)
+  {
+    return myRouter.operator [](v);
+  }
+
+private:
+  Router& myRouter;
+
+  bool isPlaceholder;
+};
+
+//===--------------------------- End of the file --------------------------===//
+#endif
